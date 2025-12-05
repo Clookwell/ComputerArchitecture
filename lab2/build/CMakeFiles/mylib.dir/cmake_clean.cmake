49c9e6635af379c1150a9bd4ca813bdbb8919622
RomanPav <65815802+Clookwell@users.noreply.github.com>
2025-12-05 22:52:52 +0200

    Add files via upload

---
+++ lab2/build/CMakeFiles/mylib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mylib.dir/link.d"
+  "CMakeFiles/mylib.dir/lib.c.o"
+  "CMakeFiles/mylib.dir/lib.c.o.d"
+  "libmylib.pdb"
+  "libmylib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mylib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
